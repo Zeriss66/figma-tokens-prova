ac9f6671bccc9acb74554d51e64e7f6a9b421819
Gabriele Motter <gabriele.motter@stride-it.com>
2022-09-21 15:28:48 +0200

    Fixing

--- sd.config.cjs
+++ sd.config.cjs
@@ -1,3 +1,7 @@
+
+const { formatHelpers } = require('style-dictionary');
+const { fileHeader, formattedVariables } = formatHelpers;
+
 module.exports = {
   source: ['sd-input/src/sd-input.json'],
   fileHeader: {
@@ -5,6 +9,21 @@ module.exports = {
       return [`Do not edit directly, this file was auto-generated`];
     },
   },
+
+  format: {
+    myCssFormat: ({ dictionary, options = {}, file }) => {
+      const selector = options.selector ? options.selector : `:root`;
+      const { outputReferences } = options;
+      dictionary.allTokens = dictionary.allTokens.filter(token => token.attributes.category !== 'tokenSetOrder');
+      return fileHeader({ file }) +
+        `${selector} {\n` +
+        formattedVariables({ format: 'css', dictionary, outputReferences }) +
+        `\n}\n`;
+    }
+  },
+
+
+
   platforms: {
     css: {
       transformGroup: 'css',
